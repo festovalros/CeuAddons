7a119f7297e901a8184d9bf6508b9ae6d0885d9f
Samuel Pavon <festovalros@gmail.com>
2018-10-24 14:10:01 -0400

    [ADD] agrega modulo de miembros

---
+++ ceu_partner/models/res_partner.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright YEAR(S), AUTHOR(S)
+# License AGPL-3. 0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    birth_date = fields.Date(string="Fecha de Nacimiento")
+    is_ucevista = fields.Boolean(string='¿Es Ucevista?')
+    blood_group = fields.Selection(string="Grupo Sanguíneo", [('A+','A+'),('A-','A-'),('B+','B+'),('B-','B-'),('O+','O+'),('O-','O-')])
+    start_year = field.Integer(string='Año de Ingreso', readonly=True)    
+
+    
