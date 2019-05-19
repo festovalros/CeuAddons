72d02b3751939e2ff44ba1a66eb9e66eb3626b64
Samuel Pavon <festovalros@gmail.com>
2019-05-19 13:30:19 -0400

    [REF] refactoriza el codigo del modelo para cumplir con las convenciones de codigo

--- ceu_opstudent/models/op_student.py
+++ ceu_opstudent/models/op_student.py
@@ -2,14 +2,15 @@
 # Copyright YEAR(S), AUTHOR(S)
 # License AGPL-3. 0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models, api
+from odoo import fields, models
 
 
 class OpStudent(models.Model):
     _inherit = 'op.student'
 
-    cedula = fields.Integer(string='Cédula', help='Ingrese su cédula en el formato 16123123')
+    cedula = fields.Integer(string='Cédula', 
+    	help='Ingrese su cédula en el formato 16123123')
     is_ucevista = fields.Boolean(string='¿Es Ucevista?')
-    start_year = fields.Integer(string='Año de Ingreso')    
-    enfermedades_alergias = fields.Text('Enfermedades y Alergias', help='Ingrese su historial de enfermedades y alergias')
-    
+    start_year = fields.Integer(string='Año de Ingreso')
+    enfermedades_alergias = fields.Text('Enfermedades y Alergias', 
+    	help='Ingrese su historial de enfermedades y alergias')
