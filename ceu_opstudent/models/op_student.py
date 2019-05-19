9e647e35fea1ed5a4512edb259f42c89e1d3a036
Samuel Pavon <festovalros@gmail.com>
2019-05-19 00:04:41 -0400

    [ADD] agrega archivo de configuracion de travis

--- ceu_opstudent/models/op_student.py
+++ ceu_opstudent/models/op_student.py
@@ -10,6 +10,6 @@ class OpStudent(models.Model):
 
     cedula = fields.Integer(string='Cédula', help='Ingrese su cédula en el formato 16123123')
     is_ucevista = fields.Boolean(string='¿Es Ucevista?')
-    start_year = fields.Integer(string='Año de Ingreso', readonly=True)    
+    start_year = fields.Integer(string='Año de Ingreso')    
     enfermedades_alergias = fields.Text('Enfermedades y Alergias', help='Ingrese su historial de enfermedades y alergias')
     
